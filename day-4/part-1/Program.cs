446888f561c7cf21367182e8fe4b4f35eb287b22
Jeroen van Leeuwen <jerryjvl@gmail.com>
2025-12-06 18:33:02 +1100

    solve: day 4

--- day-4/part-1/Program.cs
+++ day-4/part-1/Program.cs
@@ -1,3 +1,30 @@
-﻿var lines = File.ReadAllLines("../input.txt");
+﻿var lines = File.ReadAllLines("../input-full.txt");
 
-Console.WriteLine(lines.Length);
+// Expand the map with a border of dots to simplify further logic
+string[] blank = [new string('.', lines[0].Length + 2)];
+lines = blank.Concat(lines.Select(line => $".{line}.")).Concat(blank).ToArray();
+
+// Convert lines to a 2D character array
+char[][] map = lines.Select(line => line.ToArray()).ToArray();
+
+int CountRolls(int ix, int iy)
+{
+    (int,int)[] offsets = [
+        (-1, -1), (-1, 0), (-1, 1),
+        (0, -1), (0, 1),
+        (1, -1), (1, 0), (1, 1)];
+
+    return offsets.Select(
+        offset => map[iy + offset.Item2][ix + offset.Item1] == '@' ? 1 : 0
+    ).Sum();
+}
+
+long total = 0;
+for (int iy = 1; iy < lines.Length - 1; iy++)
+    for (int ix = 1; ix < lines[iy].Length - 1; ix++)
+        if (map[iy][ix] == '@')
+            if (CountRolls(ix, iy) < 4) total++;
+
+// input      = 13
+// input-full = 1486
+Console.WriteLine(total);
--- day-4/part-2/Program.cs
+++ day-4/part-2/Program.cs
@@ -1,3 +1,47 @@
-﻿var lines = File.ReadAllLines("../input.txt");
+﻿var lines = File.ReadAllLines("../input-full.txt");
 
-Console.WriteLine(lines.Length);
+// Expand the map with a border of dots to simplify further logic
+string[] blank = [new string('.', lines[0].Length + 2)];
+lines = blank.Concat(lines.Select(line => $".{line}.")).Concat(blank).ToArray();
+
+// Convert lines to a 2D character array
+char[][] map = lines.Select(line => line.ToArray()).ToArray();
+
+int CountRolls(int ix, int iy)
+{
+    (int,int)[] offsets = [
+        (-1, -1), (-1, 0), (-1, 1),
+        (0, -1), (0, 1),
+        (1, -1), (1, 0), (1, 1)];
+
+    return offsets.Select(
+        offset => map[iy + offset.Item2][ix + offset.Item1] == '@' ? 1 : 0
+    ).Sum();
+}
+
+long OnePassCountAndRemove()
+{
+    long pass = 0;
+    for (int iy = 1; iy < lines.Length - 1; iy++)
+        for (int ix = 1; ix < lines[iy].Length - 1; ix++)
+            if (map[iy][ix] == '@')
+                if (CountRolls(ix, iy) < 4)
+                {
+                    pass++;
+                    map[iy][ix] = '.';
+                }
+
+    return pass;
+}
+
+long total = 0;
+long pass = 0;
+do
+{
+    pass = OnePassCountAndRemove();
+    total += pass;
+} while (pass != 0);
+
+// input      = 43
+// input-full = 9024
+Console.WriteLine(total);
