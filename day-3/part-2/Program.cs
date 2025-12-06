18f8bae9b1394af9ec5597703cbfc7d13f6725e3
Jeroen van Leeuwen <jerryjvl@gmail.com>
2025-12-07 00:47:22 +1100

    solve: day 3

--- day-3/part-2/Program.cs
+++ day-3/part-2/Program.cs
@@ -1,3 +1,31 @@
-﻿var lines = File.ReadAllLines("../input.txt");
+﻿var lines = File.ReadAllLines("../input-full.txt");
 
-Console.WriteLine(lines.Length);
+// Problem dimension
+const int DIGITS = 12;
+var banks = lines.Select(line => line.ToArray()).ToArray();
+
+long total = 0;
+foreach (var bank in banks)
+{
+    string number = "";
+
+    var ix = 0;
+    var remainder = bank.Length - DIGITS + 1;
+    for (int iter = 0; iter < DIGITS; iter++)
+    {
+        var segment = bank.Skip(ix).Take(remainder).ToList();
+        var max = segment.Max();
+        var offset = segment.IndexOf(max);
+
+        number = number + max;
+
+        ix += offset + 1;
+        remainder -= offset;
+    }
+
+    total += long.Parse(number);
+}
+
+// input      = 3121910778619
+// input-full = 173065202451341
+Console.WriteLine(total);
