67bfe5ac5dad0db9e68fd2bad72d8b22100f4775
Jeroen van Leeuwen <jerryjvl@gmail.com>
2025-12-06 18:01:06 +1100

    chore: add cleaned up day 5

--- day-5/part-1/Program.cs
+++ day-5/part-1/Program.cs
@@ -1,3 +1,34 @@
-﻿var lines = File.ReadAllLines("../input.txt");
+﻿var lines = File.ReadAllLines("../input-full.txt");
 
-Console.WriteLine(lines.Length);
+// Process the input into more useful structures
+var emptyIx = Array.FindIndex(lines, string.IsNullOrEmpty);
+var ranges = lines.Take(emptyIx).Select(
+        rangeStr => {
+            var split = rangeStr.Split("-");
+            return (long.Parse(split[0]), long.Parse(split[1]));
+        }
+    ).ToList();
+var ingredients = lines.Skip(emptyIx + 1).Select(num => long.Parse(num)).ToList();
+
+// Sort the ranges lexicographically, and ingredients is just a simple list
+ranges.Sort();
+ingredients.Sort();
+
+long fresh = 0;
+var rangeIx = 0;
+foreach (var item in ingredients)
+{
+    // Find the first range that ends after the item, which is also the range
+    // with the earliest start due to the sort
+    while (rangeIx < ranges.Count - 1 && ranges[rangeIx].Item2 < item)
+        rangeIx++;
+
+    // If the item is in this range, it's fresh, otherwise move on...
+    if (item >= ranges[rangeIx].Item1 && item <= ranges[rangeIx].Item2)
+        fresh++;
+}
+
+// input      = 3
+// input-full = 811
+Console.WriteLine(fresh);
+
--- day-5/part-2/Program.cs
+++ day-5/part-2/Program.cs
@@ -1,3 +1,39 @@
-﻿var lines = File.ReadAllLines("../input.txt");
+﻿using System.Runtime.Serialization;
 
-Console.WriteLine(lines.Length);
+var lines = File.ReadAllLines("../input-full.txt");
+
+// Process the input into more useful structures
+var emptyIx = Array.FindIndex(lines, string.IsNullOrEmpty);
+var ranges = lines.Take(emptyIx).Select(
+        rangeStr => {
+            var split = rangeStr.Split("-");
+            return (long.Parse(split[0]), long.Parse(split[1]));
+        }
+    ).ToList();
+
+// Sort the ranges lexicographically
+ranges.Sort();
+
+// Across a list of sorted ranges, deal with overlap between subsequent ranges
+var lastRange = ranges[0];
+long total = lastRange.Item2 - lastRange.Item1 + 1;
+foreach (var range in ranges.Skip(1))
+{
+    if (range.Item1 > lastRange.Item2)
+    {
+        // No overlap, so simply add and move on
+        total += range.Item2 - range.Item1 + 1;
+        lastRange = range;
+    }
+    else if (range.Item2 > lastRange.Item2)
+    {
+        // "lastRange" overlaps "range", but does not fully enclose it
+        total += range.Item2 - lastRange.Item2;
+        lastRange = range;
+    }
+}
+
+// input      = 14
+// input-full = 338189277144473
+Console.WriteLine(total);
+
