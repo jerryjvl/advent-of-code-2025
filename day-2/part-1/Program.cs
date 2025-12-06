2bf1113d42e0dcec0983547a64a00a1ba4adfaf2
Jeroen van Leeuwen <jerryjvl@gmail.com>
2025-12-07 01:18:37 +1100

    solve: day 2

--- day-2/part-1/Program.cs
+++ day-2/part-1/Program.cs
@@ -1,3 +1,29 @@
-﻿var lines = File.ReadAllLines("../input.txt");
+﻿var lines = File.ReadAllLines("../input-full.txt");
 
-Console.WriteLine(lines.Length);
+var ranges = lines[0].Split(',').Select(range =>
+    {
+        var nums = range.Split('-');
+        return (long.Parse(nums[0]), long.Parse(nums[1]));
+    });
+
+long total = 0;
+foreach (var (start, end) in ranges)
+    for (long num = start; num <= end; num++)
+    {
+        var numstr = num.ToString();
+        int len = numstr.Length;
+
+        if (len % 2 == 0)
+        {
+            var slice = len / 2;
+            var chunks = numstr.Chunk(slice).Select(i => new string(i)).ToArray();
+            if (chunks.All(i => i == chunks[0]))
+            {
+                total += num;
+            }
+        }
+    }
+
+// input      = 1227775554
+// input-full = 12850231731
+Console.WriteLine(total);
--- day-2/part-2/Program.cs
+++ day-2/part-2/Program.cs
@@ -1,3 +1,31 @@
-﻿var lines = File.ReadAllLines("../input.txt");
+﻿var lines = File.ReadAllLines("../input-full.txt");
 
-Console.WriteLine(lines.Length);
+var ranges = lines[0].Split(',').Select(range =>
+    {
+        var nums = range.Split('-');
+        return (long.Parse(nums[0]), long.Parse(nums[1]));
+    });
+
+long total = 0;
+foreach (var (start, end) in ranges)
+    for (long num = start; num <= end; num++)
+    {
+        var numstr = num.ToString();
+        int len = numstr.Length;
+
+        for (var splits = 2; splits <= len; splits++)
+            if (len % splits == 0)
+            {
+                var slice = len / splits;
+                var chunks = numstr.Chunk(slice).Select(i => new string(i)).ToArray();
+                if (chunks.All(i => i == chunks[0]))
+                {
+                    total += num;
+                    break;
+                }
+            }
+    }
+
+// input      = 4174379265
+// input-full = 24774350322
+Console.WriteLine(total);
