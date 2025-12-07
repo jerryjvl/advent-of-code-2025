ce27b6cb7b0d23104a38b56f043677cef4291abc
Jeroen van Leeuwen <jerryjvl@gmail.com>
2025-12-07 23:35:47 +1100

    solve: day 6

--- day-6/part-1/Program.cs
+++ day-6/part-1/Program.cs
@@ -1,3 +1,23 @@
-﻿var lines = File.ReadAllLines("../input.txt");
+﻿var lines = File.ReadAllLines("../input-full.txt");
 
-Console.WriteLine(lines.Length);
+var numLines = lines.Length - 1;
+var numbers = lines.Take(numLines).Select(line =>
+    line.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+        .Select(num => long.Parse(num)).ToArray()).ToList();
+
+var operators = lines[numLines].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+long total = 0;
+for (int ix = 0; ix < operators.Length; ix++)
+{
+    long sum = operators[ix] == "+" ? 0 : 1;
+  
+    for (int iy = 0; iy < numLines; iy++)
+        sum = operators[ix] == "+" ? sum + numbers[iy][ix] : sum * numbers[iy][ix];
+
+    total += sum;
+}
+
+// input      = 4277556
+// input-full = 4387670995909
+Console.WriteLine(total);
--- day-6/part-2/Program.cs
+++ day-6/part-2/Program.cs
@@ -1,3 +1,31 @@
-﻿var lines = File.ReadAllLines("../input.txt");
+﻿var lines = File.ReadAllLines("../input-full.txt");
 
-Console.WriteLine(lines.Length);
+var numLines = lines.Length - 1;
+
+long total = 0;
+int operpos;
+int lastpos = lines[0].Length - 1;
+do
+{
+    operpos = lines[numLines].LastIndexOfAny(['+', '*'], lastpos);
+    var oper = lines[numLines][operpos];
+
+    long sum = oper == '+' ? 0 : 1;
+    for (int ix = lastpos; ix >= operpos; ix--)
+    {
+        // Parse the vertical number in column "ix"
+        string num = "";
+        for (int iy = 0; iy < lines.Length - 1; iy++)
+            num += lines[iy][ix];
+        var value = long.Parse(num);
+
+        sum = oper == '+' ? sum + value : sum * value;
+    }
+
+    total += sum;  
+    lastpos = operpos - 2;
+} while (operpos != 0);
+
+// input      = 3263827
+// input-full = 9625320374409
+Console.WriteLine(total);
