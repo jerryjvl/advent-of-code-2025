0194c764e14c24d64cbde4003d878884400df1da
Jeroen van Leeuwen <jerryjvl@gmail.com>
2025-12-07 23:07:31 +1100

    solve: day 1

--- day-1/part-1/Program.cs
+++ day-1/part-1/Program.cs
@@ -1,3 +1,19 @@
-﻿var lines = File.ReadAllLines("../input.txt");
+﻿var lines = File.ReadAllLines("../input-full.txt");
 
-Console.WriteLine(lines.Length);
+var deltas = lines.Select(line =>
+    {
+        var num = long.Parse(line.Substring(1));
+        return line[0] == 'L' ? -num : num;
+    }).ToArray();
+
+var total = 0;
+long position = 50;
+foreach (var delta in deltas)
+{
+    position = (position + delta) % 100;
+    if (position == 0) total++;
+}
+
+// input      = 3
+// input-full = 1089
+Console.WriteLine(total);
--- day-1/part-2/Program.cs
+++ day-1/part-2/Program.cs
@@ -1,3 +1,23 @@
-﻿var lines = File.ReadAllLines("../input.txt");
+﻿var lines = File.ReadAllLines("../input-full.txt");
 
-Console.WriteLine(lines.Length);
+var deltas = lines.Select(line =>
+    {
+        var num = int.Parse(line.Substring(1));
+        return line[0] == 'L' ? -num : num;
+    }).ToArray();
+
+var total = 0;
+long position = 50;
+foreach (var delta in deltas)
+{
+    var next = position + delta;
+
+    for (long num = Math.Min(position, next); num <= Math.Max(position, next); num++)
+        if (num != position && num % 100 == 0) total++;
+
+    position = (next + 1000000) % 100;
+}
+
+// input      = 6
+// input-full = 6530
+Console.WriteLine(total);
