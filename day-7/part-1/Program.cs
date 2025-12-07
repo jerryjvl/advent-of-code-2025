7009827c21cdc92ab2443bae04d1c840254db5b0
Jeroen van Leeuwen <jerryjvl@gmail.com>
2025-12-07 19:39:15 +1100

    solve:day 7

--- day-7/part-1/Program.cs
+++ day-7/part-1/Program.cs
@@ -1,3 +1,28 @@
-﻿var lines = File.ReadAllLines("../input.txt");
+﻿var lines = File.ReadAllLines("../input-full.txt");
 
-Console.WriteLine(lines.Length);
+long total = 0;
+var acc = lines[0].Select(c => c == 'S' ? '|' : '.').ToArray();
+foreach (var line in lines.Skip(1))
+{
+    var newAcc = new string('.', line.Length).ToArray();
+
+    for (var ix = 0; ix < line.Length; ix++)
+        if (acc[ix] == '|')
+            switch (line[ix])
+            {
+                case '.':
+                    newAcc[ix] = '|';
+                    break;
+                case '^': 
+                    newAcc[ix - 1] = '|';
+                    newAcc[ix + 1] = '|';
+                    total++;
+                    break;
+            }
+
+    acc = newAcc;
+}
+
+// input      = 21
+// input-full = 1638
+Console.WriteLine(total);
--- day-7/part-2/Program.cs
+++ day-7/part-2/Program.cs
@@ -1,3 +1,21 @@
-﻿var lines = File.ReadAllLines("../input.txt");
+﻿var lines = File.ReadAllLines("../input-full.txt");
 
-Console.WriteLine(lines.Length);
+// Work from the bottom to the top, accumulating universes, starting with
+// 1 universe in each column.
+var width = lines[0].Length;
+long[] universes = Enumerable.Repeat<long>(1, width).ToArray();
+
+foreach (var line in lines.Reverse())
+{
+    var newUniverses = (long[])universes.Clone();
+
+    for (int ix = 0; ix < width; ix++)
+        if (line[ix] == '^')
+            newUniverses[ix] = universes[ix - 1] + universes[ix + 1];
+
+    universes = newUniverses;
+}
+
+// input      = 40
+// input-full = 7759107121385
+Console.WriteLine(universes[lines[0].IndexOf('S')]);
